ad9dd2578f95dfcf9a0fae3ea6944115abaa6f40
pellul <49030609+raven99p@users.noreply.github.com>
2021-03-04 00:24:34 +0200

    Delete test.groovy

--- test.groovy
+++
@@ -1,9 +0,0 @@
-import groovy.sql.Sql
-
-def db = [url:'jdbc:postgresql://localhost/emp', user:'me', password:'password', driver:'org.postgresql.Driver']
-def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
-
-println 'Some GR8 projects:'
-sql.eachRow('select * from employee') { row ->
-    println " $row.id ${row.name.padRight(10)} ($row.date_of_birth)"
-}
