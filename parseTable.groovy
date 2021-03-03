5d706221a75a91f38eeb3fcd6a9e1da992517717
pellul <49030609+raven99p@users.noreply.github.com>
2021-03-04 00:26:28 +0200

    Update parseTable.groovy

--- parseTable.groovy
+++ parseTable.groovy
@@ -3,7 +3,7 @@ import groovy.sql.Sql
 def db = [url:'jdbc:postgresql://localhost/emp', user:'me', password:'password', driver:'org.postgresql.Driver']
 def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
-println 'Some GR8 projects:'
+println 'Table: employee, Database: emp'
 sql.eachRow('select * from employee') { row ->
     println " $row.id ${row.name.padRight(10)} ($row.date_of_birth)"
 }
